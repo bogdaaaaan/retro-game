cf71c48b343639c9f21afb04d91de4a1e81dcebf
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2021-09-21 19:27:21 +0300

    bfs algorithm update

--- src/PathFinding.js
+++ src/PathFinding.js
@@ -1,5 +1,3 @@
-import { coordsFromPos, GRID_SIZE } from './setup.js';
-
 export default class PathFinding {
     constructor() {
         this.algorithm = null;
@@ -9,47 +7,59 @@ export default class PathFinding {
         //directions
         // right, left, down, up
         let dir = [ [0,1], [0,-1], [1,0], [-1,0]];
-    
         //queue
         const q = [];
-         
+        
         //insert the top right corner.
         q.push(from);
         
         // path
         const path = [];
         path.push(from)
-    
+
         //until queue is empty
-        while(q.length > 0) {       
+        while(q.length > 0) {      
             let p = q.shift();
-    
+
             //mark as visited
-            arr[p[0]][p[1]] = -1;
-             
+            arr[p[0]][p[1]] = 1;
+            
             //destination is reached.
             if(p[0] === to[0] && p[1] === to[1]) {
-                console.log(path);
+                //console.log(path);
                 return true;
             } 
-    
+
             //check all four directions
             for(let i = 0; i < 4 ;i++) {
                 //using the direction array
                 let [a, b] = [ p[0] + dir[i][0], p[1] + dir[i][1]];
                 if (a < 0 || b < 0 || a >= arr.length || b >= arr.length) continue;
-    
+
                 //not blocked and valid
-                if(arr[a][b] !== -1 ) {
+                if(arr[a][b] !== 1 &&  arr[a][b] !== 9) {
+
                     if(a === to[0] && b === to[1]) {
-                        console.log(path);
+                        path.push([a,b]);
+                        //console.log(path);
                         return true;
                     }
-                    path.push([a,b]);
-                    q.push([a,b]);
+
+                    let flag = false;
+                    for (let i = 0; i < path.length; i++) {
+                        if (path[i][0] === a && path[i][1] === b) {
+                            flag = true;
+                        }
+                    }
+
+                    if (!flag) {
+                        path.push([a,b]);
+                        q.push([a,b]);
+                    }
                 }
             }
         }
+        //console.log(path);
         return false;
     }
 }
