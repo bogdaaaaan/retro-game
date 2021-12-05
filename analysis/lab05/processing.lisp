fd9954d4c9a8ad9887b7b0f0b0d5cdcd0c8362e1
Bogdan Glechkovskiy <bogdan.glechkovskiy@gmail.com>
2021-12-05 03:11:28 +0200

    lab05 done

--- analysis/lab05/processing.lisp
+++ analysis/lab05/processing.lisp
@@ -8,54 +8,54 @@
 (print dataset)
 
 ; function to get scores from dataset
-(defun get-score-list (dataset)
-    (let ((score-list ()))
+(defun get_score_list (dataset)
+    (let ((score_list ()))
         (dolist (el (cdr dataset))
-            ; get 2 column from dataset and write all data from it to score-list
-            (setq score-list (append score-list (list (parse-float (nth 2 el))))))
-        (return-from get-score-list score-list)
+            ; get 2 column from dataset and write all data from it to score_list
+            (setq score_list (append score_list (list (parse-float (nth 2 el))))))
+        (return-from get_score_list score_list)
     )
 )
 
 ; function to get time from dataset
-(defun get-time-list (dataset)
-    (let ((time-list()))
+(defun get_time_list (dataset)
+    (let ((time_list()))
         (dolist (el (cdr dataset))
-            ; get 1 column from dataset and write all data from it to time-list
-            (setq time-list (append time-list(list (parse-float (nth 1 el))))))
-        (return-from get-time-list time-list)
+            ; get 1 column from dataset and write all data from it to time_list
+            (setq time_list (append time_list(list (parse-float (nth 1 el))))))
+        (return-from get_time_list time_list)
     )
 )
 
 ; function to calculate expected value
-(defun get-expected-value (list-of-numbers)
+(defun get_expected_value (list_of_numbers)
     (let ((sum 0))
-        (dolist (el list-of-numbers)
+        (dolist (el list_of_numbers)
             (setq sum (+ sum el)))
         ; expected value  = sum of all elements in list divided by length of list
-        (return-from get-expected-value (/ sum (list-length list-of-numbers)))
+        (return-from get_expected_value (/ sum (list-length list_of_numbers)))
     )
 )
 
 ; function to calculate dispersion
-(defun get-dispersion (list-of-numbers)
+(defun get_dispersion (list_of_numbers)
     ; get expected value of scores
-    (let ((list-in-square ()) (mean (get-expected-value list-of-numbers)))
-        (dolist (el list-of-numbers)
+    (let ((list_in_square ()) (mean (get_expected_value list_of_numbers)))
+        (dolist (el list_of_numbers)
             ; raise all elements from list to the power of two
-            (setq list-in-square (append list-in-square (list (* el el))))
+            (setq list_in_square (append list_in_square (list (* el el))))
         )
         ; dispersion = expected value of scores powered of two - expected value raised to power of two  
-        (return-from get-dispersion (- (get-expected-value list-in-square) (* mean mean)))
+        (return-from get_dispersion (- (get_expected_value list_in_square) (* mean mean)))
     )
 )
 
 ; print data
-(print (get-score-list dataset))
-(print (get-time-list dataset))
+(print (get_score_list dataset))
+(print (get_time_list dataset))
 
 (print "Expected value of time:")
-(print (get-expected-value (get-time-list dataset)))
+(print (get_expected_value (get_time_list dataset)))
 
 (print "Dispersion of score:")
-(print (get-dispersion (get-score-list dataset)))
+(print (get_dispersion (get_score_list dataset)))
